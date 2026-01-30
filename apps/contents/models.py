d405eaf094f1247e044847ce0da25c5cba6e95c6
kaira ooki <kaira.ooki0623@gmail.com>
2026-01-30 17:55:45 +0900

    contents

--- apps/contents/models.py
+++ apps/contents/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class MenuPost(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    ingredients = models.TextField()
+    recipe = models.TextField()
+    image = models.ImageField(upload_to='menus/', blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+

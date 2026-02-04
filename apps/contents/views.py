87a5a38da37e45608ec72b636f1e991b7cd551a9
HJ-jyo <jyo1222lucky90@gmail.com>
2026-02-04 22:09:28 +0900

    投稿ができるように修正

--- apps/contents/views.py
+++ apps/contents/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import MenuPost
+from .forms import MenuPostForm
 
 # 投稿一覧画面
 def index_view(request):
@@ -19,7 +20,7 @@ def create_view(request):
         # フォームから送られてきたデータを保存
         post = MenuPost.objects.create(
             user=request.user,
-            title=request.POST.get('name'),    
+            title=request.POST.get('title'),   
             ingredients=request.POST.get('ingredients'),
             ingredients_num=request.POST.get('ingredients_num'),
             recipe=request.POST.get('recipe'), 
@@ -30,3 +31,23 @@ def create_view(request):
 
     # 投稿用ページを表示（ファイル名はプロジェクトに合わせて post.html などに修正してください）
     return render(request, 'contents/post.html')
+
+# 新規投稿画面
+@login_required
+def create_view(request):
+    if request.method == 'POST':
+        # フォームを使ってデータを受け取る（画像も忘れずに！）
+        form = MenuPostForm(request.POST, request.FILES)
+        
+        # データに不備がないかチェック
+        if form.is_valid():
+            post = form.save(commit=False) # まだDBには保存しない
+            post.user = request.user       # ユーザー情報を追加
+            post.save()                    # ここで保存！
+            return redirect('contents:detail', pk=post.pk)
+    else:
+        # 最初の表示用（空のフォームを作る）
+        form = MenuPostForm()
+
+    # テンプレートに form を渡す
+    return render(request, 'contents/post.html', {'form': form})
